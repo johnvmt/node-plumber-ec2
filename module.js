febf6811dce7d1c0e3bf1153e76a0b14346a6402
johnvmt <johnvmt@gmail.com>
2015-05-04 17:16:26 -0400

    First stable version

--- module.js
+++ module.js
@@ -10,10 +10,29 @@ function Ec2Platform(plumber, config) {
 	this.ec2 = new AWS.EC2();
 }
 
+Ec2Platform.prototype.instanceId = function(callback) {
+	// get the ID for the running instance (machine), as returned by startInstance
+	var self = this;
+
+	if(typeof self._instanceId != "string") {
+		var request = require("request");
+		request('http://169.254.169.254/latest/meta-data/instance-id', function(error, response, body) {
+			if(!error && response.statusCode == 200) {
+				self._instanceId = body;
+				callback(null, body);
+			}
+			else
+				callback(error, null);
+		});
+	}
+	else
+		callback(null, self._instanceId);
+};
+
 Ec2Platform.prototype.startInstance = function(params, callback) {
 	// TODO: do something to params?
 	this.ec2.runInstances(params, function(err, data) {
-		if (err)
+		if(err)
 			callback(err, null);
 		else {
 			var instanceId = data.Instances[0].InstanceId;
